6d1b14e7094e51761f4fad9d2512594ff42555f6
asivishkin <a.sivishkin@loyaltyplant.com>
2019-06-17 01:10:37 +0300

    2_print_result

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,11 @@ public class Main {
         solution q1 = new solution(4.5,6.0,2.0);
         solution q2 = new solution(4.5,6.0,3.0);
         solution q3 = new solution(4.5,7.0,2.0);
-        System.out.println(q1.quadraticSolution());
-        System.out.println(q2.quadraticSolution());
-        System.out.println(q3.quadraticSolution());
-
+        q1.printResult();
+        q1.setA(6.7);
+        q1.printResult();
+        q1.printResult();
+//        q2.printResult();
+//        q3.printResult();
     }
 }
